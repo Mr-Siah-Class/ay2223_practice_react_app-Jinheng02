fefc255ece4d6e37e133fd10cde6ff83ea81950a
Jinheng02 <jinheng20.21@ichat.sp.edu.sg>
2022-07-07 15:27:06 +0800

    Added FooterBar.js #2

---
+++ react-app-example/src/FooterBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class FooterBar extends React.Component
+{
+    // eslint-disable-next-line no-useless-constructor
+    constructor() {
+        super()
+    }
+
+    render()
+    {
+        return (
+            <div id='Footer'>
+                <p>Realization Lion &copy; 2022. All Rights Reserved.</p>
+            </div>
+        );
+    }
+}
+
+export default FooterBar;

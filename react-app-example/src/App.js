490c98fae404d6ea6f6a08bd06e4a6432285b6ea
Jinheng02 <jinheng20.21@ichat.sp.edu.sg>
2022-07-07 15:29:25 +0800

    Added App.js #2

--- react-app-example/src/App.js
+++ react-app-example/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+import NavBar from './NavBar';
+import FooterBar from './FooterBar';
+import HomeScreen from './HomeScreen';
+import NoScreen from './NoScreen';
+import ProductsScreen from './ProductsScreen';
+import SingleProductScreen from './SingleProductScreen.js';
+
+const ProductIdWrapper = () => {
+  const { productId } = useParams();
+  return <SingleProductScreen productId={productId} />;
+};
+
 class App extends React.Component
 {
+    // eslint-disable-next-line no-useless-constructor
     constructor() {
         super()
     }
@@ -11,7 +31,18 @@ class App extends React.Component
     {
         return (
             <div>
-                <h1>Hello React!</h1>
+                <NavBar/>
+
+                <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<ProductIdWrapper/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+
+                <FooterBar/>
             </div>
         );
     }
